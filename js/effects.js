3b8e003120159b91a01268ce59931c32d641f904
h27096 <h27096@gmail.com>
2026-07-17 16:50:40 -0500

    Add screenStatic function to adjust body opacity

--- js/effects.js
+++ js/effects.js
@@ -1,1 +1,13 @@
+function screenStatic(){
 
+    document.body.style.opacity=".92";
+
+    setTimeout(()=>{
+
+        document.body.style.opacity="1";
+
+    },80);
+
+}
+
+setInterval(screenStatic,5000);
